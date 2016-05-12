24f64c2271dc23f2954a5d7bd4f271f0311c6c71
duki994 <duki994@gmail.com>
2016-05-12 13:45:22 +0200

    drivers: snesorhub: set proximity wakelock to 1*HZ again
    Error was in my sensor calibration, not wl timeout

--- drivers/sensorhub/stm/ssp_input.c
+++ drivers/sensorhub/stm/ssp_input.c
@@ -434,7 +434,7 @@ void report_prox_data(struct ssp_data *data, struct sensor_value *proxdata)
 		(!proxdata->prox[0]) + 1);
 	input_sync(data->prox_input_dev);
 
-	wake_lock_timeout(&data->ssp_wake_lock, 3 * HZ);
+	wake_lock_timeout(&data->ssp_wake_lock, 1 * HZ);
 }
 
 void report_prox_raw_data(struct ssp_data *data,
