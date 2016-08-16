fabfd35b71070d65130b4514bbe841993fc12372
duki994 <duki994@gmail.com>
2016-08-17 01:30:00 +0200

    mm/zswap: expose some parameters via sysfs

--- mm/zswap.c
+++ mm/zswap.c
@@ -82,12 +82,12 @@ static atomic_t zswap_zero_pages = ATOMIC_INIT(0);
 **********************************/
 /* Enable/disable zswap (enabled by default, fixed at boot for now) */
 static bool zswap_enabled = 1;
-module_param_named(enabled, zswap_enabled, bool, 0);
+module_param_named(enabled, zswap_enabled, bool, 0644);
 
 /* Compressor to be used by zswap (fixed at boot for now) */
 #define ZSWAP_COMPRESSOR_DEFAULT "lz4"
 static char *zswap_compressor = ZSWAP_COMPRESSOR_DEFAULT;
-module_param_named(compressor, zswap_compressor, charp, 0);
+module_param_named(compressor, zswap_compressor, charp, 0644);
 
 /* The maximum percentage of memory that the compressed pool can occupy */
 static unsigned int zswap_max_pool_percent = 20;
